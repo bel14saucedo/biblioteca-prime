a606cb24bcfcb2486b99561c20605c55bfd9603c
Anasaucedo <belen14saucedo@gmail.com>
2022-09-01 14:45:42 -0400

    Tarea 1 Angular (Parte 1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import {MenuItem, PrimeIcons} from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'biblioteca-prime';
+ items: MenuItem[]=[
+  {
+    label: "Biblioteca",
+    icon: PrimeIcons.BOOK,
+    items: [
+      { 
+        label: "Libros",
+        icon: PrimeIcons.BOOKMARK
+      },
+      {
+         label: "Autores",
+         icon: PrimeIcons.USERS
+        }
+    ]
+  }
+ ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule} from 'primeng/button';
+import {MenubarModule} from 'primeng/menubar';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
